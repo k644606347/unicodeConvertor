cdaf12e38ad1996b45140f5f671578090da61184
k644606347 <k644606347@126.com>
2018-05-29 22:40:19 +0800

    update format method

--- unicodeConvertor.js
+++ unicodeConvertor.js
@@ -1,7 +1,13 @@
 
 let unicodeConvertor = {
 	format: (str) => {
-		return str.split('').map((n) => '\\u' + n.charCodeAt().toString(16)).join('');
+		str = String(str);
+
+		let result = '';
+		for (let i = 0; i < str.length; i ++) {
+			result += '\\u' + str[i].charCodeAt().toString(16);
+		}
+		return result;
 	},
 	parse: (unicode) => {
 		return unicode.split('\\u').filter((n) => n != '').map(n => String.fromCodePoint(Number('0x' + n))).join('');
